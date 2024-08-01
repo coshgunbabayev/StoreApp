97d562e1a584070c9b596dded26522d4704c957d
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-08-01 05:10:37 +0400

    models (Store User) done

---
+++ models/store.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+import { ObjectId } from 'mongodb';
+import validator from 'validator';
+
+const storeSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+        trim: true,
+        minlength: [3, 'Name must be at least 3 characters long'],
+        maxlength: [50, 'Name cannot be more than 50 characters long']
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        trim: true,
+        unique: true,
+        validate: [validator.isEmail, 'Please enter a valid email address']
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        trim: true,
+        minlength: [8, 'Password must be at least 8 characters long'],
+        maxlength: [50, 'Password cannot be more than 50 characters long']
+    },
+
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+
+    profilePictureId: {
+        type: String,
+        default: ''
+    },
+
+    about: {
+        type: String,
+        trim: true,
+        maxlength: [200, 'About cannot be more than 200 characters long'],
+        default: ''
+    },
+
+    address: {
+        type: String,
+        trim: true,
+        maxlength: [50, 'Address cannot be more than 50 characters long'],
+        default: ''
+    },
+
+    contactNumber: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: [50, 'Contact number cannot be more than 50 characters long'],
+        default: ''
+    }
+});
+
+const Store = mongoose.model('Store', storeSchema);
+
+export default Store;
---
+++ models/user.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+        minlength: [3, 'Name must be at least 3 characters long'],
+        maxlength: [50, 'Name cannot be more than 50 characters long'],
+        validate: {
+            validator: validator.isAlpha,
+            message: 'Name should only contain alphabetical characters'
+        }
+    },
+
+    surname: {
+        type: String,
+        required: [true, 'Please enter a surname'],
+        minlength: [3, 'Surname must be at least 3 characters long'],
+        maxlength: [50, 'Surname cannot be more than 50 characters long'],
+        validate: {
+            validator: validator.isAlpha,
+            message: 'Surname should only contain alphabetical characters'
+        }
+    },
+
+    username: {
+        type: String,
+        required: [true, 'Please enter a username'],
+        unique: true,
+        minlength: [3, 'Username must be at least 3 characters long'],
+        maxlength: [50, 'Username cannot be more than 50 characters long'],
+        validate: {
+            validator: validator.isAlphanumeric,
+            message: 'Username should only contain alphanumeric characters'
+        }
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please enter a valid email address'
+        }
+    },
+
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        trim: true,
+        minlength: [8, 'Password must be at least 8 characters long'],
+        maxlength: [50, 'Password cannot be more than 50 characters long']
+    },
+
+    profilePicture: {
+        type: String,
+        default: ''
+    },
+
+    profilePictureId: {
+        type: String,
+        default: ''
+    },
+
+    address: {
+        type: String,
+        trim: true,
+        maxlength: [50, 'Address cannot be more than 50 characters long'],
+        default: ''
+    },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
